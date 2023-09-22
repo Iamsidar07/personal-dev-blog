49fb3fd5b89328b4a72fde8dd5e953cdf9b36ce3
iamsidar07 <ms8460149@gmail.com>
2023-09-22 16:50:11 +0530

    added sanity studio title

--- app/(studio)/layout.tsx
+++ app/(studio)/layout.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
 import "../globals.css";
-
+export const metadata: Metadata = {
+    title: "Studio| Manoj DEV blog",
+    description: "Manoj's developer blog where I share my learning."
+}
 export default function StudioLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
